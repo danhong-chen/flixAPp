7934da964cbcf38c696da298192368e8e0d2eb17
Dan Hong Chen <danhong.chen@ww.com>
2021-09-11 23:49:45 -0400

    first commit

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'flisterApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for flisterApp
+  pod "AlamofireImage"
+
+
+  target 'flisterAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'flisterAppUITests' do
+    # Pods for testing
+  end
+
+end
---
+++ flisterApp/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  flisterApp
+//
+//  Created by Vivian Chen on 9/11/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
